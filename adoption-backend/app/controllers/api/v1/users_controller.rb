3f1992d77b0ef4760c2a6766b7e2c705ce33fc71
github username <github email address>
2019-08-26 15:23:00 -0400

    update

--- adoption-backend/app/controllers/api/v1/users_controller.rb
+++ adoption-backend/app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,28 @@ class Api::V1::UsersController < ApplicationController
       render json: user
     end
 
+    def edit
+      @user = User.find(params[:id])
+      render json: user
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to user_path(@user)
+      else
+        render json: user
+      end
+    end
+
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path
+        render json: user
+    end
+
 
 
 
